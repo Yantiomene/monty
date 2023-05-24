9614315a43a974d04235e2ce5e5295c680eec357
Yaninthe Tiomene <yan2016tiomene@gmail.com>
2023-05-24 14:23:04 -0700

    [Feature] Adding a function to execute push instruction

---
+++ push.c
@@ -0,0 +1,30 @@
+#include "monty.h"
+
+/**
+ * push - pushez data to a stack
+ * @head: pointer to the pointer the head of the stack
+ * @line: current line number
+ *
+ */
+void push(stack_t **head, unsigned int line)
+{
+	int n, i;
+
+	if (!gvar.arg)
+	{
+		fprintf(stderr, "L%u: usage: push integer\n", line);
+		free_var();
+		exit(EXIT_FAILURE);
+	}
+	for (i = 0; gvar.arg[i] != '\0'; i++)
+	{
+		if (!isdigit(gvar.arg[i]) && gvar.arg[i] != '-')
+		{
+			fprintf(stderr, "L%u: usage: push integer\n", line);
+			free_var();
+			exit(EXIT_FAILURE);
+		}
+	}
+	n = atoi(gvar.arg);
+	add_dnodeint(head, n);
+}
