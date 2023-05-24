e208dce6768fd51b4d98a530308f060757b40610
Yaninthe Tiomene <yan2016tiomene@gmail.com>
2023-05-24 15:35:51 -0700

    [Feature] Adding a function to execute the nopp instruction

---
+++ nop.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+/**
+ * nop - do nothing
+ * @head: pointer to the pointer to the head of the stack
+ * @line: current line number
+ *
+ * Return: nothing
+ */
+void nop(stack_t **head, unsigned int line)
+{
+	(void)head;
+	(void)line;
+}
