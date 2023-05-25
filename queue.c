b3fd943db1d555bf21d6b235588cdbc57d44a348
Yaninthe Tiomene <yan2016tiomene@gmail.com>
2023-05-25 07:13:52 -0700

    [Feature] Adding a function to execute the queue instruction

---
+++ queue.c
@@ -0,0 +1,16 @@
+#include "monty.h"
+
+/**
+ * _queue - sets the format of the data to a queue
+ * @head: pointer to pointer to the head of the queue
+ * @line: current line number
+ *
+ * Return: nothing
+ */
+void _queue(stack_t **head, unsigned int line)
+{
+	(void)head;
+	(void)line;
+
+	gvar.s_q = 0;
+}
