75f9cd262707a132f0b4635c9f76086fafbd8cb8
Ige Victor <victorkoladeige@gmail.com>
2023-05-25 14:18:10 -0700

    add.c

--- add.c
+++ add.c
@@ -1,7 +1,7 @@
 #include "monty.h"
 
 /**
- * _add - devides the second top element of the stack by the top element
+ * _add - computes the rest of the division of 2nd element by the 1st element
  * @head: pointer to the pointer to the head of the stack
  * @line: current line number
  *
@@ -9,9 +9,24 @@
  */
 void _add(stack_t **head, unsigned int line)
 {
-	if (*stack == NULL || (*stack)->next == NULL)
+	stack_t *tmp;
+	unsigned int count = 0;
+
+	tmp = *head;
+	while (tmp)
+	{
+		count++;
+		tmp = tmp->next;
+	}
+	if (count < 2)
 	{
 		fprintf(stderr, "L%u: can't add, stack too short\n", line);
+		free_var();
+		exit(EXIT_FAILURE);
+	}
+	if ((*head)->n == 0)
+	{
+		fprintf(stderr, "L%u: division by zero\n", line);
 		free_var();
 		exit(EXIT_FAILURE);
 	}
