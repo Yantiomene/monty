ffcc6b309275dc7a7475706a6f2eb14cda13df4d
Yaninthe Tiomene <yan2016tiomene@gmail.com>
2023-05-25 07:13:25 -0700

    [Feature] Adding a function to execute the stack instruction

---
+++ stack.c
@@ -0,0 +1,16 @@
+#include "monty.h"
+
+/**
+ * stack - set the format of the data to a stack
+ * @head: pointer to pointer to the head of the stack
+ * @line: current line number
+ *
+ * Return: nothing
+ */
+void stack(stack_t **head, unsigned int line)
+{
+	(void)head;
+	(void)line;
+
+	gvar.s_q = 1;
+}
